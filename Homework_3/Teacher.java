bbc536905d7921e67ddc9209e83a4901ae666159
Pavel <elemir2010@gmail.com>
2023-06-16 20:37:30 +0300

    add solutions for Hometask_3

--- Homework_3/Teacher.java
+++ Homework_3/Teacher.java
@@ -1,52 +1,78 @@
 package Homework_3;
-//В этом классе есть четыре поля: name, email, subjects и yearsOfExperience. 
-//Конструктор принимает значения для каждого поля. 
-//Также есть геттеры и сеттеры для каждого поля, а также метод getTeachingExperience, 
-//который возвращает строку, описывающую опыт преподавания. 
-//Есть также метод teach, который выводит сообщение "Обучает...
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+//Создан новый класс Учитель и сюда включены два компратора - один сравнивает зарплату и выводит  список от большей к меньшей
+//второй компаратор выводит самых оптынх и самых молодых сотрудников
+
 public class Teacher {
-    private String name;
-    private String email;
-    private String[] subjects;
-    private int yearsOfExperience;
+    private String firstName;
+    private String lastName;
+    private int salary;
+    private int experience;
 
-    public Teacher(String name, String email, String[] subjects, int yearsOfExperience) {
-        this.name = name;
-        this.email = email;
-        this.subjects = subjects;
-        this.yearsOfExperience = yearsOfExperience;
+    public Teacher(String firstName, String lastName, int salary, int experience) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.experience = experience;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getEmail() {
-        return email;
+    public String getLastName() {
+        return lastName;
     }
 
-    public String[] getSubjects() {
-        return subjects;
+    public int getSalary() {
+        return salary;
     }
 
-    public int getYearsOfExperience() {
-        return yearsOfExperience;
+    public int getExperience() {
+        return experience;
     }
 
-    public void setYearsOfExperience(int yearsOfExperience) {
-        this.yearsOfExperience = yearsOfExperience;
-    }
+    public static void main(String[] args) {
+        List<Teacher> teachers = new ArrayList<>();
+        teachers.add(new Teacher("Иван", "Петров", 50000, 5));
+        teachers.add(new Teacher("Евгения", "Николаева", 60000, 10));
+        teachers.add(new Teacher("Семенов", "Семен", 40000, 2));
+        teachers.add(new Teacher("Алиса", "Михайлова", 55000, 7));
+        teachers.add(new Teacher("Андрей", "Смирнов", 45000, 3));
 
-    public String getTeachingExperience() {
-        if (yearsOfExperience == 1) {
-            return "1 год педагогического опыта";
-        } else {
-            return yearsOfExperience + " лет педагогического стажа";
+        // компаратор для сравнения зарплаты
+        Comparator<Teacher> salaryComparator = new Comparator<Teacher>() {
+            @Override
+            public int compare(Teacher t1, Teacher t2) {
+                return t2.getSalary() - t1.getSalary();
+            }
+        };
+
+        // компаратор для сравнения опыта работы
+        Comparator<Teacher> experienceComparator = new Comparator<Teacher>() {
+            @Override
+            public int compare(Teacher t1, Teacher t2) {
+                if (t1.getExperience() > t2.getExperience()) {
+                    System.out.println("Самый опытный сотрудник: " + t1.getFirstName() + " " + t1.getLastName() + ", стаж работы: " + t1.getExperience());
+                } else if (t1.getExperience() < t2.getExperience()) {
+                    System.out.println("Самый молодой сотрудник: " + t1.getFirstName() + " " + t1.getLastName() + ", стаж работы: " + t1.getExperience());
+                }
+                return t2.getExperience() - t1.getExperience();
+            }
+        };
+
+        // сортировка по зарплате
+        Collections.sort(teachers, salaryComparator);
+        System.out.println("Список учителей, отсортированный по зарплате:");
+        for (Teacher teacher : teachers) {
+            System.out.println(teacher.getFirstName() + " " + teacher.getLastName() + ", зарплата: " + teacher.getSalary());
         }
-    }
 
-    public void teach() {
-        System.out.println("Обучает...");
+        // сортировка по опыту работы
+        Collections.sort(teachers, experienceComparator);
     }
-
 }
